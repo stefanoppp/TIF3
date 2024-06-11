b54a4601c25b8564cbc05c11e74c811a45a8ea03
Stefano <spstefanoperetti17@gmail.com>
2024-06-11 15:30:37 -0300

    Mostrar datos modo automatico

--- training/views.py
+++ training/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from training_time_series import main
+import plotly.graph_objects as go
+import plotly.io as pio
+import pandas as pd
 
 def home(request):
-    return render(request,"training/home.html")
+    return render(request, "training/home.html")
 
 @login_required
 def train(request):
-    return render(request,"training/train.html")
+    return render(request, "training/train.html")
 
 @login_required
 def developer_mode(request):
@@ -15,8 +19,29 @@ def developer_mode(request):
 
 @login_required
 def automatic_mode(request):
-    resultado=4
-    return render(request, "training/automatic_mode.html", {'result': resultado})
+    predictions = main()  
+    df_predictions = predictions.reset_index()
+    df_predictions.columns = ['Periodo', 'Prediccion']
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=df_predictions['Periodo'],
+        y=df_predictions['Prediccion'],
+        mode='lines+markers',
+        name='Predicción'
+    ))
+
+    # Añadir título y etiquetas
+    fig.update_layout(
+        title='Predicciones',
+        xaxis_title='Perido a partir de Abril-2024',
+        yaxis_title='Valor predicho',
+        template='plotly_dark'
+    )
+
+    # Convertir el gráfico a HTML
+    graph_div = pio.to_html(fig, full_html=False)
+
+    return render(request, "training/automatic_mode.html", {'result': df_predictions.to_html(index=False), 'graph_div': graph_div})
 
 def exit(request):
     logout(request)
